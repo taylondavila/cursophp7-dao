8247688ce4adcb9a117494db9c5969307c2e1858
Taylon <taylondavila@gmail.com>
2020-10-05 13:49:28 -0300

    metodos de lista

--- class/sql.php
+++ class/sql.php
@@ -6,7 +6,7 @@ class Sql extends PDO {
 
 	public function __construct(){
 
-		$this->conn = new PDO("mysql:host=localhost;dbname=dbphp7", "root", "");
+		$this->conn = new PDO("mysql:host=localhost;dbname=dbphp7", "root", "root");
 
 	}
 
--- index.php
+++ index.php
@@ -1,11 +1,47 @@
-<?php
+<?php 
 
-require_once ("config.php");
+require_once("config.php");
 
-$root = new usuario();
+//Carrega um usuário
+//$root = new Usuario();
+//$root->loadbyId(3);
+//echo $root;
 
-$root->loadyId(2);
+//Carrega uma lista de usuários
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
-echo $root;
+//Carrega uma lista de usuários buscando pelo login
+//$search = Usuario::search("jo");
+//echo json_encode($search);
 
+//carrega um usuário usando o login e a senha
+//$usuario = new Usuario();
+//$usuario->login("root", "!@#$");
+
+//echo $usuario;
+/*
+//Criando um novo usuário
+$aluno = new Usuario("aluno", "@lun0");
+$aluno->insert();
+echo $aluno;
+*/
+/*
+//Alterar um usuário
+$usuario = new Usuario();
+
+$usuario->loadById(8);
+
+$usuario->update("professor", "!@#$%¨&*");
+
+echo $usuario;
+*/
+
+$usuario = new Usuario();
+
+$usuario->loadById(7);
+
+$usuario->delete();
+
+echo $usuario;
  ?>
